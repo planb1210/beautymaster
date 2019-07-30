2a55251785393b06548eafdb4a1e6187f9605d5d
perishi <mxuser@mx>
2019-07-29 23:48:59 -0400

    edit users

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,8 @@ return array(
 	'news/([0-9]+)' => 'news/view/$1',
 	'news' => 'news/index', 
 	'admin' => 'admin/index',
+	'users/([0-9]+)' => 'users/edit/$1',
 	'users' => 'users/index',
+	
 	'authorize' => 'authorize/index'
 	);
--- views/users/index.php
+++ views/users/index.php
@@ -8,7 +8,30 @@
 		<div class="container-wrapper container">
 			<?php echo $this->menu; ?>	
 			<div class="content">
-				Тут должен быть функционал добавления/редактирования/удаления пользователя
+				<form method="post" action="/users/3">
+					Отобразить список пользователей<input type="submit" value="Отобразить" name="submit"/>
+				</form>
+				<form method="post" action="/users/0">
+					<table border=1>
+						<tr><td>Имя</td><td><input type=text name=name size=10</td></tr>
+						<tr><td>email</td><td><input type=text name=email size=10></td></tr>
+						<tr><td>Пароль</td><td><input type=text name=password size=10></td></tr>
+					</table>
+					<input type=submit  value="Создать" name="submit">
+				</form>
+				<form method="post" action="/users/1">
+					<table border=1>
+						<tr><td>Id</td><td><input type=text name=id size=10</td></tr>
+						<tr><td>Имя</td><td><input type=text name=name size=10</td></tr>
+						<tr><td>email</td><td><input type=text name=email size=10></td></tr>
+						<tr><td>Пароль</td><td><input type=password name=password size=10></td></tr>
+					</table>
+					<input type=submit  value="Редактировать" name="submit">
+				</form>
+				<form method="post" action="/users/3">
+					Чтобы удалить пользователя, введите его id <input type="text" name="id" />
+					<input type="submit" value="Удалить" name="submit"/>
+				</form>
 			</div>
 		</div>
 		<?php echo $this->footer; ?>
